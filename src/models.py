924d41b1d586f80e762e5111ce9c386d9cb23faf
Martin-cedres <martinfernandocedres@gmail.com>
2024-11-17 19:16:09 +0000

    MODELO DATOS STARWARS MARTIN CEDRES

--- src/models.py
+++ src/models.py
@@ -7,24 +7,46 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class User(Base):
+    __tablename__ = 'user'
+    # Defino la columna de la tabla user
+    # Cada columna también es un atributo de instancia normal de Python.
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False, unique=True)
+    email = Column(String(50), nullable=False, unique=True)
+    
+
+class Characters(Base):
+    __tablename__ = 'characters'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(50), nullable=False)
+    height = Column(String(50), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Planets(Base):
+    __tablename__ = 'planets'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50), nullable=False)
+    climate = Column(String(100), nullable=False)
+    
 
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    chararcters_id = Column(Integer, ForeignKey('characters.id')) # Con Foreingkey establece el vinculo logico entre dos tablas 
+    characters = relationship(Characters) # El relationship utiliza la relacion par afacilitar el acceso al codigo (obtiene los objetos de las tablas) 
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+    planets = relationship(Planets)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    
     def to_dict(self):
         return {}
 
